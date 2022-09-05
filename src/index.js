4a53c224005b0a44fc43ed29517ef59c6c9c1566
nepoz <hinokamitube77@gmail.com>
2022-09-04 23:19:00 -0500

    Elements are snapshots of UI; we must recreate element each time to update

--- src/index.js
+++ src/index.js
@@ -6,8 +6,21 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const title = <h1>Planner</h1>
+const not_title = <h3>WRONG HEADING WRONG HEADING</h3>
 
 root.render(title)
+
+
+// Swap between title we want and title we DON'T want based on value of flag
+let flag = true
+const swap = () => {
+  const element = flag ? title : not_title
+  flag = !flag
+  root.render(element)
+}
+
+// Calls swap to run ever 1000ms = 1 sec
+setInterval(swap, 1000)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
