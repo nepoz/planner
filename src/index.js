2fe22d3b0690c2dc8bbaf9acb9b17bbaffb38e51
nepoz <hinokamitube77@gmail.com>
2022-09-04 23:39:51 -0500

    Build components!

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { render } from '@testing-library/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const title = "Planner"
@@ -10,17 +11,33 @@ const not_title = "WRONG HEADING WRONG HEADING"
 
 root.render(title)
 
-
-// Swap between title we want and title we DON'T want based on value of flag
-let flag = true
-const swap = () => {
-  const element = <h1> Title: {flag ? title : not_title} </h1>
-  flag = !flag
-  root.render(element)
+// Create Title component -- ALL COMPONENTS MUST BE CAPITALIZED
+const Title = (props) => {
+  return <h1>{props.text}</h1> 
 }
 
-// Calls swap to run ever 1000ms = 1 sec
-setInterval(swap, 1000)
+// Create Description component -- ALL COMPONENTS MUST BE CAPITALIZED
+const Description = (props) => {
+  return <p>{props.text}</p>
+}
+
+// Create Task component -- made up of a title and a description
+const Task = (props) => {
+  return (
+  <div id="TaskBox">
+    <Title text={props.titleText} />
+    <Description text={props.descriptionText} />
+  </div>
+  )
+}
+
+// create an element -- a single task. Remember empty JSX tags may be closed as in XML
+const heading = "Planner Test"
+const description = "This is a test component that uses small components to build a bigger component!"
+const element = <Task titleText={heading} descriptionText={description} />
+
+// render the test element
+root.render(element)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
