ec11fa48904fb9d02bf318319becfd6e5efb2cd5
nepoz <44448021+nepoz@users.noreply.github.com>
2022-09-05 20:57:45 -0500

    Move components from index to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Create Title component -- ALL COMPONENTS MUST BE CAPITALIZED
+const Title = (props) => {
+  return <h1>{props.text}</h1> 
+}
+
+// Create Description component -- ALL COMPONENTS MUST BE CAPITALIZED
+const Description = (props) => {
+  return <p>{props.text}</p>
+}
+
+// Create Task component -- made up of a title and a description
+const Task = (props) => {
+  return (
+  <div id="TaskBox">
+    <Title text={props.titleText} />
+    <Description text={props.descriptionText} />
+  </div>
+  )
+}
+
+// create an element -- a single task. Remember empty JSX tags may be closed as in XML
+const heading = "Planner Test"
+const description = "This is a test component that uses small components to build a bigger component!"
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <Task titleText={heading} descriptionText={description} />
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,34 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import { render } from '@testing-library/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// Create Title component -- ALL COMPONENTS MUST BE CAPITALIZED
-const Title = (props) => {
-  return <h1>{props.text}</h1> 
-}
-
-// Create Description component -- ALL COMPONENTS MUST BE CAPITALIZED
-const Description = (props) => {
-  return <p>{props.text}</p>
-}
-
-// Create Task component -- made up of a title and a description
-const Task = (props) => {
-  return (
-  <div id="TaskBox">
-    <Title text={props.titleText} />
-    <Description text={props.descriptionText} />
-  </div>
-  )
-}
-
-// create an element -- a single task. Remember empty JSX tags may be closed as in XML
-const heading = "Planner Test"
-const description = "This is a test component that uses small components to build a bigger component!"
-const element = <Task titleText={heading} descriptionText={description} />
-
-// render the test element
-root.render(element)
+// render the app 
+root.render(<App />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
